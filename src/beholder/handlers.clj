f81da4bd3afac8f3c6c54676b57f7a98de79c719
Peter Taoussanis <peter@taoensso.com>
2023-06-22 10:58:52 +0200

    Remove unnecessary deps, problem still exists

--- src/beholder/handlers.clj
+++ src/beholder/handlers.clj
@@ -1,9 +1,3 @@
 (ns beholder.handlers
   (:require
-   [clj-http.client :as client]
-   [clojure.string :refer [split]]
-   [compojure.core :refer :all]
-   [compojure.route :as route]
-   [ring.middleware.defaults :refer [wrap-defaults]]
-   [ring.util.http-response :refer :all]
    [taoensso.timbre :as log]))
